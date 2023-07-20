e62a1360bb064a50e64a78751bc4784ce6fa6858
Kushal Bajracharya <bajrakus20@gmail.com>
2023-07-20 22:08:50 +0545

    added:providedbetternameforroutes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { getAllUserController, createUserController } = require('../controllers/user-controller');
 const router = express.Router();
 
-router.get('/useraauxa', getAllUserController); // Get all users
+router.get('/getuser', getAllUserController); // Get all users
 router.post('/adduser', createUserController); // Create a new user
-router.delete('/usersbanxa', createUserController); // Create a new user
-router.put('/usersbanxa', createUserController); // Create a new user
+router.delete('/deleteuser', createUserController); // Delete a new user
+router.put('/updateuser', createUserController); // Create a new user
 router.get('/usersname', createUserController); // Create a new user
-router.get('/usersnamekathmandu', createUserController); // Create a new user
+router.get('/usersemailandpassword', createUserController); // Create a new user
 
 module.exports = router;
