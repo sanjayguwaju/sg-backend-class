c0b5a15c8a64a76e6d1ad28d41d95fad24a40ad9
sanjayguwaju <sanjayguwaju@gmail.com>
2023-07-21 22:03:59 +0545

    created routes for users with all CRUD API

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { getAllUserController, createUserController } = require('../controllers/user-controller');
+const { getAllUserController, createUserController,updateUserController,deleteUserController } = require('../controllers/user-controller');
 const router = express.Router();
 
 router.get('/getuser', getAllUserController); // Get all users
 router.post('/adduser', createUserController); // Create a new user
-router.delete('/deleteuser', createUserController); // Delete a new user
-router.put('/updateuser', createUserController); // Create a new user
+router.delete('/deleteuser/:id',deleteUserController ); // Delete a new user
+router.put('/updateuser/:id', updateUserController); // Create a new user
 router.get('/usersname', createUserController); // Create a new user
 router.get('/usersemailandpassword', createUserController); // Create a new user
 
